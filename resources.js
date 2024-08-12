a23be652a93478c72cbd9ee771b34c8f3846bb6a
aleoweb123 <aleoweb123@gmail.com>
2024-08-12 11:54:24 +0800

    languages

---
+++ resources.js
@@ -0,0 +1,23 @@
+import en from "./en.json";
+import zh from "./zh.json";
+import ja from "./ja.json";
+import zhTW from "./zh-TW.json";
+import ko from "./ko_KR.json";
+
+export const resources = {
+  en: {
+    translation: en,
+  },
+  zh: {
+    translation: zh,
+  },
+  ja: {
+    translation: ja,
+  },
+  zhTW: {
+    translation: zhTW,
+  },
+  ko: {
+    translation: ko,
+  },
+};
